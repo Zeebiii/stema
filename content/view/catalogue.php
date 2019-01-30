8967e680d37d1a87d054a5f2dd6028479b23cc67
lizardman2297 <45826441+lizardman2297@users.noreply.github.com>
2019-01-30 21:37:32 +0100

    Update catalogue.php

--- content/view/catalogue.php
+++ content/view/catalogue.php
@@ -2,10 +2,13 @@
 <html xmlns="http://www.w3.org/1999/xhtml" lang="fr" xml:lang="en">
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <link rel="stylesheet" href="../contenu/stema.css"/>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/solid.css" integrity="sha384-+0VIRx+yz1WBcCTXBkVQYIBVNEFH1eP6Zknm16roZCyeNg2maWEpk/l/KsyFKs7G" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/fontawesome.css" integrity="sha384-jLuaxTTBR42U2qJ/pm4JRouHkEDHkVqH0T1nyQXn1mZ7Snycpf6Rl25VBNthU4z0" crossorigin="anonymous">  
-
+    <link rel="stylesheet" href="../css/stema.css"/>
+    <link rel="stylesheet" href="../css/owlCarousel/owl.carousel.min.css"/>
+    <link rel="stylesheet" href="../css/owlCarousel/owl.theme.default.min.css"/>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/solid.css" integrity="sha384-+0VIRx+yz1WBcCTXBkVQYIBVNEFH1eP6Zknm16roZCyeNg2maWEpk/l/KsyFKs7G" crossorigin="anonymous"/>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/fontawesome.css" integrity="sha384-jLuaxTTBR42U2qJ/pm4JRouHkEDHkVqH0T1nyQXn1mZ7Snycpf6Rl25VBNthU4z0" crossorigin="anonymous"/>  
+    <script src="../js/jquery.min.js"></script>
+    <script src="../js/owl.carousel.min.js"></script>
     <title>Bijouterie Stema</title>
 </head>
 <body>
@@ -15,39 +18,67 @@
         <div id="header">
 
             <div id="logo">
-                <h2>Bijouterie STEMA</h2>
+                <img src="../files/image/logo.png" alt="LogoMarque">
             </div>
 
             <div id="log">
-                login
+
+                <form action="../method/logOn.php" method="post">
+
+                    <fieldset id="fieldsetLog">
+
+                        <label for="pseudo">Pseudo</label>
+                        <!-- <br/> -->
+                        <input type="text" name="pseudo" id="pseudo"/>
+                        <!-- <br/> -->
+                        <label for="password">password</label>
+                        <!-- <br/> -->
+                        <input type="password" name="pass" id="pass"/>
+                        <!-- <br/> -->
+
+                        <input type="submit" value="Log on"/>
+                        <br/>
+                        <a href="logIn.php">Log in</a>
+                    
+                    </fieldset>
+                </form>
+
+
             </div>
 
             <div id="menu">
 
                 <ul>
                     <li class="angleGauchce">
-                        <a href="../">Acceuil</a> 
+                        Acceuil
+                    </li>
+                    <li class="dropdown">
+                        <a href="catalogue.php">Catalogue <i class="fas fa-arrow-down arrowDown"></i></a>  
+
+                        <ul>
+                            <li><a href="view/baguePerle.php">Nos bagues perle</a></li>
+                            <li><a href="view/pierreFine.php">Nos pierres fine</a></li>
+                            <li><a href="view/bagueCeramique.php">Nos bagues ceramique</a></li>
+                            <li><a href="view/bagueOr.php" class="basDropdown">Nos bagues en or</a></li>
+                        </ul>
                     </li>
                     <li>
-                        Catalogue
+                        <a href="view/boutique.php">Boutique</a>
                     </li>
                     <li>
-                        <a href="boutique.html">Boutique</a>
-                    </li>
-                    <li>
-                        <a href="chiffre.html">Chiffres</a> 
+                        <a href="view/chiffre.php">Chiffres</a> 
                     </li>
                     <li class="angleDroit">
-                        <a href="contact.html">Contact</a> 
+                        <a href="view/contact.php">Contact</a> 
                     </li>
                 </ul>
 
-            </div>
+            </div> 
 
         </div>
 
 
-    </div>
 
+    </div>
 </body>
 </html>
