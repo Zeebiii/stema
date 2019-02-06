8d87ec12220454a1fd6f13e55d3585a0a48a269b
lizardman2297 <45826441+lizardman2297@users.noreply.github.com>
2019-02-06 16:16:59 +0100

    sliderCatalogue

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 $(document).ready(function(){
     $('.owl-carousel').owlCarousel({
-        items:5,
+        items:4,
         margin:30,
         loop:true,
         autoplay:true,
-        autoplayTimeout:2500,
+        autoplayTimeout:5000,
         autoplayHoverPause:true,
         nav:true,
         navText: ['<i class="fas fa-chevron-left fa-2x flecheGauche"></i>','<i class="fas fa-chevron-right fa-2x flecheDroite"></i>']
     });
   });
+
---
+++ sliderCatalogue.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        items:4,
+        margin:30,
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:5000,
+        autoplayHoverPause:true,
+        dots: false
+    });
+  });
