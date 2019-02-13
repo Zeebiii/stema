efaaf9aae6a1e1a53715d6e2d8a3682c15ee1ffd
lizardman2297 <45826441+lizardman2297@users.noreply.github.com>
2019-02-13 16:55:20 +0100

    cata suite

--- content/js/appCatalogue.js
+++ content/js/appCatalogue.js
@@ -2,10 +2,19 @@ $(document).ready(function(){
     $('.owl-carousel').owlCarousel({
         items:4,
         margin:30,
-        loop:true,
+        loop:false,
         autoplay:true,
         autoplayTimeout:5000,
         autoplayHoverPause:true,
-        dots: false
+        dots: false,
+        rewind:true,
     });
   });
+
+
+
+$(document).ready(function(){
+    lightbox.option({
+        'positionFromTop': 100,
+      });
+});
