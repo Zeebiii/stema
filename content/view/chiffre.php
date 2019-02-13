3dc2902d9355ad4a13338da71952f3c9d3204c2b
lizardman2297 <45826441+lizardman2297@users.noreply.github.com>
2019-02-13 20:03:18 +0100

    menu

--- content/view/chiffre.php
+++ content/view/chiffre.php
@@ -2,10 +2,16 @@
 <html xmlns="http://www.w3.org/1999/xhtml" lang="fr" xml:lang="en">
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <link rel="stylesheet" href="../contenu/stema.css"/>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/solid.css" integrity="sha384-+0VIRx+yz1WBcCTXBkVQYIBVNEFH1eP6Zknm16roZCyeNg2maWEpk/l/KsyFKs7G" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/fontawesome.css" integrity="sha384-jLuaxTTBR42U2qJ/pm4JRouHkEDHkVqH0T1nyQXn1mZ7Snycpf6Rl25VBNthU4z0" crossorigin="anonymous">  
-
+    <link rel="stylesheet" href="../css/stema.css"/>
+    <link rel="stylesheet" href="../css/owlCarousel/owl.carousel.min.css"/>
+    <link rel="stylesheet" href="../css/owlCarousel/owl.theme.default.min.css"/>
+    <link rel="stylesheet" href="../css/lightbox/lightbox.css"/>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/solid.css" integrity="sha384-+0VIRx+yz1WBcCTXBkVQYIBVNEFH1eP6Zknm16roZCyeNg2maWEpk/l/KsyFKs7G" crossorigin="anonymous"/>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/fontawesome.css" integrity="sha384-jLuaxTTBR42U2qJ/pm4JRouHkEDHkVqH0T1nyQXn1mZ7Snycpf6Rl25VBNthU4z0" crossorigin="anonymous"/>  
+    <script src="../js/jquery.min.js"></script>
+    <script src="../js/owl.carousel.min.js"></script>
+    <script src="../js/appCatalogue.js"></script>
+    <script src="../js/lightbox.js"></script>
     <title>Bijouterie Stema</title>
 </head>
 <body>
@@ -28,17 +34,24 @@
                     <li class="angleGauchce">
                         <a href="../">Acceuil</a> 
                     </li>
-                    <li>
-                        <a href="catalogue.html">Catalogue</a>
+                    <li class="dropdown">
+                        <a href="catalogue.php">Catalogue <i class="fas fa-arrow-down arrowDown"></i></a>
+
+                        <ul>
+                            <li><a href="baguePerle.php"></a>Nos bagues perle</li>
+                            <li><a href="pierreFine.php">Nos pierres fine</a></li>
+                            <li><a href="bagueCeramique.php">¨Nos bagues ceramique</a></li>
+                            <li><a href="bagueOr.php">Nos bagues en or</a></li>
+                        </ul>
                     </li>
                     <li>
-                        <a href="boutique.html">Boutique</a>
+                        <a href="boutique.php">Boutique</a>
                     </li>
                     <li>
-                        Chiffres 
+                        Chiffres
                     </li>
                     <li class="angleDroit">
-                        <a href="contact.html">Contact</a> 
+                        <a href="contact.php">Contact</a> 
                     </li>
                 </ul>
 
