c7f194f8cfc94be6c46a8141e5a1717319e88054
lizardman2297 <45826441+lizardman2297@users.noreply.github.com>
2019-02-13 15:13:02 +0100

    lightbox slider cata ( pas fini)

--- content/view/catalogue.php
+++ content/view/catalogue.php
@@ -5,11 +5,13 @@
     <link rel="stylesheet" href="../css/stema.css"/>
     <link rel="stylesheet" href="../css/owlCarousel/owl.carousel.min.css"/>
     <link rel="stylesheet" href="../css/owlCarousel/owl.theme.default.min.css"/>
+    <link rel="stylesheet" href="../css/lightbox/lightbox.css"/>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/solid.css" integrity="sha384-+0VIRx+yz1WBcCTXBkVQYIBVNEFH1eP6Zknm16roZCyeNg2maWEpk/l/KsyFKs7G" crossorigin="anonymous"/>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/fontawesome.css" integrity="sha384-jLuaxTTBR42U2qJ/pm4JRouHkEDHkVqH0T1nyQXn1mZ7Snycpf6Rl25VBNthU4z0" crossorigin="anonymous"/>  
     <script src="../js/jquery.min.js"></script>
     <script src="../js/owl.carousel.min.js"></script>
     <script src="../js/appCatalogue.js"></script>
+    <script src="../js/lightbox.js"></script>
     <title>Bijouterie Stema</title>
 </head>
 <body>
@@ -100,12 +102,12 @@
 
                         </div>
                         <div class="owl-carousel owl-theme sliderCatalogue">
-                            <div class="item"><a href="#"><img src="../files/imgCatalogue/Bague_Marae_Grande.png" alt=""></a></div>
-                            <div class="item"><a href="#"><img src="../files/imgCatalogue/Bague_Only_Tahiti_Grande.png" alt=""></a></div>
-                            <div class="item"><a href="#"><img src="../files/imgCatalogue/Bague_Paea_Grande.png" alt=""></a></div>
-                            <div class="item"><a href="#"><img src="../files/imgCatalogue/Bague_Morgana_Grande.png" alt=""></a></div>
-                            <div class="item"><a href="#"><img src="../files/imgCatalogue/Bague_Griffon_Perle_Grande.png" alt=""></a></div>
-                            <div class="item"><a href="#"><img src="../files/imgCatalogue/Bague_Electre_Or_Blanc_Grande.png" alt=""></a></div>
+                            <div class="item"><a href="../files/imgCatalogue/Bague_Marae_Grande.png" data-lightbox="Marae"><img src="../files/imgCatalogue/Bague_Marae_Grande.png" alt=""></a></div>
+                            <div class="item"><a href="../files/imgCatalogue/Bague_Only_Tahiti_Grande.png" data-lightbox="Marae"><img src="../files/imgCatalogue/Bague_Only_Tahiti_Grande.png" alt=""></a></div>
+                            <div class="item"><a href="../files/imgCatalogue/Bague_Paea_Grande.png" data-lightbox="Marae"><img src="../files/imgCatalogue/Bague_Paea_Grande.png" alt=""></a></div>
+                            <div class="item"><a href="../files/imgCatalogue/Bague_Morgana_Grande.png" data-lightbox="Marae"><img src="../files/imgCatalogue/Bague_Morgana_Grande.png" alt=""></a></div>
+                            <div class="item"><a href="../files/imgCatalogue/Bague_Griffon_Perle_Grande.png" data-lightbox="Griffon_Perle"><img src="../files/imgCatalogue/Bague_Griffon_Perle_Grande.png" alt=""></a></div>
+                            <div class="item"><a href="../files/imgCatalogue/Bague_Electre_Or_Blanc_Grande.png" data-lightbox="Electre_Or_Blanc"><img src="../files/imgCatalogue/Bague_Electre_Or_Blanc_Grande.png" alt=""></a></div>
                         </div>
                     </div>
                 </div>
@@ -126,10 +128,10 @@
                         </div>
 
                         <div class="owl-carousel sliderBox sliderCatalogue">
-                            <div class="item"><a href="#"><img src="../files/imgCatalogue/Bague_lune_violette_Grande.png" alt=""></a></div>
-                            <div class="item"><a href="#"><img src="../files/imgCatalogue/Bague_fleur_cognac_Grande.png" alt=""></a></div>
-                            <div class="item"><a href="#"><img src="../files/imgCatalogue/Bague_néon_améthyste_Grande.png" alt=""></a></div>
-                            <div class="item"><a href="#"><img src="../files/imgCatalogue/Bague_bora_palme_Grande.png" alt=""></a></div>
+                            <div class="item"><a href="../files/imgCatalogue/Bague_lune_violette_Grande.png" data-lightbox="lune_violette"><img src="../files/imgCatalogue/Bague_lune_violette_Grande.png" alt=""></a></div>
+                            <div class="item"><a href="../files/imgCatalogue/Bague_fleur_cognac_Grande.png" data-lightbox="fleur_cognac"><img src="../files/imgCatalogue/Bague_fleur_cognac_Grande.png" alt=""></a></div>
+                            <div class="item"><a href="../files/imgCatalogue/Bague_neon_amethyste_Grande.png" data-lightbox="neon_amethyste"><img src="../files/imgCatalogue/Bague_neon_amethyste_Grande.png" alt=""></a></div>
+                            <div class="item"><a href="../files/imgCatalogue/Bague_bora_palme_Grande.png" data-lightbox="bora_palme"><img src="../files/imgCatalogue/Bague_bora_palme_Grande.png" alt=""></a></div>
                         </div>
                     </div>
                 </div>
@@ -150,12 +152,12 @@
                         </div>
 
                         <div class="owl-carousel owl-theme sliderCatalogue">
-                            <div class="item"><a href="#"><img src="../files/imgCatalogue/Bague_Marae_Grande.png" alt=""></a></div>
-                            <div class="item"><a href="#"><img src="../files/imgCatalogue/Bague_Only_Tahiti_Grande.png" alt=""></a></div>
-                            <div class="item"><a href="#"><img src="../files/imgCatalogue/Bague_Paea_Grande.png" alt=""></a></div>
-                            <div class="item"><a href="#"><img src="../files/imgCatalogue/Bague_Morgana_Grande.png" alt=""></a></div>
-                            <div class="item"><a href="#"><img src="../files/imgCatalogue/Bague_Griffon_Perle_Grande.png" alt=""></a></div>
-                            <div class="item"><a href="#"><img src="../files/imgCatalogue/Bague_Electre_Or_Blanc_Grande.png" alt=""></a></div>
+                            <div class="item"><a href="../files/imgCatalogue/Bague_black_sparkle_Grande.png" data-lightbox="black_sparkle"><img src="../files/imgCatalogue/Bague_black_sparkle_Grande.png" alt=""></a></div>
+                            <div class="item"><a href="../files/imgCatalogue/Bague_Xevelite_noire_diamants_Grande.png" data-lightbox="Xevelite_noire"><img src="../files/imgCatalogue/Bague_Xevelite_noire_diamants_Grande.png" alt=""></a></div>
+                            <div class="item"><a href="../files/imgCatalogue/Bague_Nyx_Grande.png" data-lightbox="Nyx"><img src="../files/imgCatalogue/Bague_Nyx_Grande.png" alt=""></a></div>
+                            <div class="item"><a href="../files/imgCatalogue/Bague_Hypnos_Grande.png" data-lightbox="Hypnos"><img src="../files/imgCatalogue/Bague_Hypnos_Grande.png" alt=""></a></div>
+                            <div class="item"><a href="../files/imgCatalogue/Bague_White_dawn_Grande.png" data-lightbox="White_dawn"><img src="../files/imgCatalogue/Bague_White_dawn_Grande.png" alt=""></a></div>
+                            <div class="item"><a href="../files/imgCatalogue/Bague_Gourmette_Grande.png" data-lightbox="Gourmette"><img src="../files/imgCatalogue/Bague_Gourmette_Grande.png" alt=""></a></div>
                         </div>
                     </div>
                 </div>
@@ -178,12 +180,11 @@
                         </div>
 
                         <div class="owl-carousel owl-theme sliderCatalogue">
-                            <div class="item"><a href="#"><img src="../files/imgCatalogue/Bague_Marae_Grande.png" alt=""></a></div>
-                            <div class="item"><a href="#"><img src="../files/imgCatalogue/Bague_Only_Tahiti_Grande.png" alt=""></a></div>
-                            <div class="item"><a href="#"><img src="../files/imgCatalogue/Bague_Paea_Grande.png" alt=""></a></div>
-                            <div class="item"><a href="#"><img src="../files/imgCatalogue/Bague_Morgana_Grande.png" alt=""></a></div>
-                            <div class="item"><a href="#"><img src="../files/imgCatalogue/Bague_Griffon_Perle_Grande.png" alt=""></a></div>
-                            <div class="item"><a href="#"><img src="../files/imgCatalogue/Bague_Electre_Or_Blanc_Grande.png" alt=""></a></div>
+                            <div class="item"><a href="../files/imgCatalogue/Bague_Angel_Or_Jaune_Grande.png" data-lightbox="Angel_Or_Jaune"><img src="../files/imgCatalogue/Bague_Angel_Or_Jaune_Grande.png" alt=""></a></div>
+                            <div class="item"><a href="../files/imgCatalogue/Bague_Angel_Or_Blanc_Grande.png" data-lightbox="Angel_Or_Blanc"><img src="../files/imgCatalogue/Bague_Angel_Or_Blanc_Grande.png" alt=""></a></div>
+                            <div class="item"><a href="../files/imgCatalogue/Bague_adorable_jaune_Grande.png" data-lightbox="adorable_jaune"><img src="../files/imgCatalogue/Bague_adorable_jaune_Grande.png" alt=""></a></div>
+                            <div class="item"><a href="../files/imgCatalogue/Bague_ellipses_Grande.png" data-lightbox="ellipses"><img src="../files/imgCatalogue/Bague_ellipses_Grande.png" alt=""></a></div>
+                            <div class="item"><a href="../files/imgCatalogue/Bague_brindille_or_blanc_Grande.png" data-lightbox="brindille_or"><img src="../files/imgCatalogue/Bague_brindille_or_blanc_Grande.png" alt=""></a></div>
                         </div>
                     </div>
                     
