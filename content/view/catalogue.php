f6cfcaebdbc4859ff3789139ff08436c7a332f4b
Zeebiii <mathias.msl@outlook.fr>
2019-02-13 14:28:20 +0100

    Ajout des balise a pour les images de la galerie

--- content/view/catalogue.php
+++ content/view/catalogue.php
@@ -126,10 +126,10 @@
                         </div>
 
                         <div class="owl-carousel sliderBox sliderCatalogue">
-                            <div class="item"><img src="../files/imgCatalogue/Bague_lune_violette_Grande.png" alt=""></div>
-                            <div class="item"><img src="../files/imgCatalogue/Bague_fleur_cognac_Grande.png" alt=""></div>
-                            <div class="item"><img src="../files/imgCatalogue/Bague_néon_améthyste_Grande.png" alt=""></div>
-                            <div class="item"><img src="../files/imgCatalogue/Bague_bora_palme_Grande.png" alt=""></div>
+                            <div class="item"><a href="#"><img src="../files/imgCatalogue/Bague_lune_violette_Grande.png" alt=""></a></div>
+                            <div class="item"><a href="#"><img src="../files/imgCatalogue/Bague_fleur_cognac_Grande.png" alt=""></a></div>
+                            <div class="item"><a href="#"><img src="../files/imgCatalogue/Bague_néon_améthyste_Grande.png" alt=""></a></div>
+                            <div class="item"><a href="#"><img src="../files/imgCatalogue/Bague_bora_palme_Grande.png" alt=""></a></div>
                         </div>
                     </div>
                 </div>
@@ -150,12 +150,12 @@
                         </div>
 
                         <div class="owl-carousel owl-theme sliderCatalogue">
-                            <div class="item"><img src="../files/imgCatalogue/Bague_Marae_Grande.png" alt=""></div>
-                            <div class="item"><img src="../files/imgCatalogue/Bague_Only_Tahiti_Grande.png" alt=""></div>
-                            <div class="item"><img src="../files/imgCatalogue/Bague_Paea_Grande.png" alt=""></div>
-                            <div class="item"><img src="../files/imgCatalogue/Bague_Morgana_Grande.png" alt=""></div>
-                            <div class="item"><img src="../files/imgCatalogue/Bague_Griffon_Perle_Grande.png" alt=""></div>
-                            <div class="item"><img src="../files/imgCatalogue/Bague_Electre_Or_Blanc_Grande.png" alt=""></div>
+                            <div class="item"><a href="#"><img src="../files/imgCatalogue/Bague_Marae_Grande.png" alt=""></a></div>
+                            <div class="item"><a href="#"><img src="../files/imgCatalogue/Bague_Only_Tahiti_Grande.png" alt=""></a></div>
+                            <div class="item"><a href="#"><img src="../files/imgCatalogue/Bague_Paea_Grande.png" alt=""></a></div>
+                            <div class="item"><a href="#"><img src="../files/imgCatalogue/Bague_Morgana_Grande.png" alt=""></a></div>
+                            <div class="item"><a href="#"><img src="../files/imgCatalogue/Bague_Griffon_Perle_Grande.png" alt=""></a></div>
+                            <div class="item"><a href="#"><img src="../files/imgCatalogue/Bague_Electre_Or_Blanc_Grande.png" alt=""></a></div>
                         </div>
                     </div>
                 </div>
@@ -178,12 +178,12 @@
                         </div>
 
                         <div class="owl-carousel owl-theme sliderCatalogue">
-                            <div class="item"><img src="../files/imgCatalogue/Bague_Marae_Grande.png" alt=""></div>
-                            <div class="item"><img src="../files/imgCatalogue/Bague_Only_Tahiti_Grande.png" alt=""></div>
-                            <div class="item"><img src="../files/imgCatalogue/Bague_Paea_Grande.png" alt=""></div>
-                            <div class="item"><img src="../files/imgCatalogue/Bague_Morgana_Grande.png" alt=""></div>
-                            <div class="item"><img src="../files/imgCatalogue/Bague_Griffon_Perle_Grande.png" alt=""></div>
-                            <div class="item"><img src="../files/imgCatalogue/Bague_Electre_Or_Blanc_Grande.png" alt=""></div>
+                            <div class="item"><a href="#"><img src="../files/imgCatalogue/Bague_Marae_Grande.png" alt=""></a></div>
+                            <div class="item"><a href="#"><img src="../files/imgCatalogue/Bague_Only_Tahiti_Grande.png" alt=""></a></div>
+                            <div class="item"><a href="#"><img src="../files/imgCatalogue/Bague_Paea_Grande.png" alt=""></a></div>
+                            <div class="item"><a href="#"><img src="../files/imgCatalogue/Bague_Morgana_Grande.png" alt=""></a></div>
+                            <div class="item"><a href="#"><img src="../files/imgCatalogue/Bague_Griffon_Perle_Grande.png" alt=""></a></div>
+                            <div class="item"><a href="#"><img src="../files/imgCatalogue/Bague_Electre_Or_Blanc_Grande.png" alt=""></a></div>
                         </div>
                     </div>
                     
